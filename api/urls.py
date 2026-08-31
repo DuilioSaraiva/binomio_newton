b93e4b3382490c08e56b45081888e183d81d94d9
17rzpio <rodrigo_pioner@yahoo.com.br>
2026-08-31 20:05:06 -0300

    endpoint get do banco de dados

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import hello_world
+from .views import usuarios
 
 urlpatterns = [
     path("hello/", hello_world),
+    path("usuarios/", usuarios),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Usuario
 
 def hello_world(request):
     return HttpResponse("Hello World!")
 # Create your views here.
+
+def usuarios(request):
+    usuarios = Usuario.objects.all()
+
+    dados = []
+
+    for usuario in usuarios:
+        dados.append({
+            "id": usuario.id,
+            "nome": usuario.nome
+        })
+
+    return JsonResponse(dados, safe=False)
